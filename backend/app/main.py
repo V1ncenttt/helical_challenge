ecbc0b83ca45c278cae6beed79f9aaf2f5be8501
Lefeuve Vincent <vincent.lefeuve@student-cs.fr>
2025-06-28 22:07:50 +0100

    Created modelregistry at startup time

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes import upload, workflow, meta
 from db.init_db import init_database
-
+from ml.model_registry import ModelRegistry
 app = FastAPI()
 
 app.include_router(upload.router)
@@ -11,4 +11,5 @@ app.include_router(meta.router)
 @app.on_event("startup")
 async def startup_event():
     init_database()
+    ModelRegistry()
     print("✅ Database initialized successfully.")
