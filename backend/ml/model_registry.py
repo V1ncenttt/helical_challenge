61c2f98803bd2b66c0207d8cc26fb429d41746b0
Lefeuve Vincent <vincent.lefeuve@student-cs.fr>
2025-06-28 22:07:28 +0100

    Fixed model loading

--- backend/ml/model_registry.py
+++ backend/ml/model_registry.py
@@ -3,8 +3,12 @@
 from transformers import AutoModel, AutoTokenizer
 from helical.models.scgpt import scGPT, scGPTConfig
 from helical.models.geneformer import Geneformer, GeneformerConfig
-from ml.classification_head import ClassificationHead
 import torch
+import torch.nn as nn
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+models_dir = os.path.join(BASE_DIR, "ml", "parameters")
 
 def singleton(cls):
     instances = {}
@@ -21,8 +25,10 @@ class ModelRegistry:
     def __init__(self):
         
         self.input_shape = 512
-        self.num_classes = 10
+        self.num_classes = 6
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.params_head_scgpt = os.path.join(models_dir, "head_model_scgpt.pth")
+        self.params_head_geneformer = os.path.join(models_dir, "head_model_geneformer.pth")
 
         self.embedding_models = {}
         self.classification_models = {}
@@ -35,10 +41,13 @@ class ModelRegistry:
         3: 'MYELOID',
         4: 'PROGENITOR',
         5: 'STROMA'}
+        
+        self.num_classes = 6
+        
         print("✅ Models loaded successfully----------.")
 
     def _load_models(self):
-        
+        print("Loading models...")
         scgpt_config = scGPTConfig(batch_size=50, device=self.device)
         scgpt = scGPT(configurer = scgpt_config)
         self.embedding_models["scgpt"] = scgpt
@@ -47,14 +56,18 @@ class ModelRegistry:
         geneformer = Geneformer(configurer = geneformer_config)
         self.embedding_models["geneformer"] = geneformer
 
-        scgpt_classification_head = ClassificationHead(self.input_shape, self.num_classes)
-        scgpt_classification_head.load_state_dict("path/to/scgpt_classification_head.pth")
+        scgpt_classification_head = self._get_head_model()
+        scgpt_classification_head.load_state_dict(torch.load(self.params_head_scgpt))
         scgpt_classification_head.to(self.device)
         scgpt_classification_head.eval()
         self.classification_models["scgpt"] = scgpt_classification_head
         
-        geneformer_classification_head = ClassificationHead(self.input_shape, self.num_classes)
-        geneformer_classification_head.load_state_dict("path/to/geneformer_classification_head.pth")
+        geneformer_classification_head = self._get_head_model()
+        geneformer_classification_head.load_state_dict(torch.load(self.params_head_geneformer))
+        #Check if the state_dict file exists at "../ml/parameters/head_model_geneformer.pth"
+        if not os.path.exists(self.params_head_geneformer):
+            raise FileNotFoundError("State dict file not found.")
+
         geneformer_classification_head.to(self.device)
         geneformer_classification_head.eval()
         self.classification_models["geneformer"] = geneformer_classification_head
@@ -67,3 +80,14 @@ class ModelRegistry:
     
     def get_label(self, id):
         return self.id2label.get(id, "Unknown")
+    
+    def _get_head_model(self):
+        return nn.Sequential(
+            nn.Linear(self.input_shape, 128),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(128, 32),
+        nn.ReLU(),
+        nn.Dropout(0.4),
+        nn.Linear(32, self.num_classes)
+        )
